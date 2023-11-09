ebc527bc52687b15531aef91f125000c7cfdc6e2
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2023-11-09 08:59:52 -0300

    animate tab item

--- src/components/SettingsTabs/index.tsx
+++ src/components/SettingsTabs/index.tsx
@@ -1,21 +1,73 @@
 'use client'
 import * as Tabs from '@radix-ui/react-tabs'
 import { TabItem } from './tabitem'
+import { useState } from 'react'
 
 export function SettingsTabs() {
+  const [currentTab, setCurrentTab] = useState('tab1')
   return (
-    <Tabs.Root>
+    <Tabs.Root value={currentTab} onValueChange={setCurrentTab}>
       <Tabs.List className="mt-6 flex w-full items-center gap-4 border-b border-zinc-200">
-        <TabItem key={'tab1'} title="My Details" isSelected value="tab1" />
-        <TabItem key={'tab2'} title="Profile" value="tab2" />
-        <TabItem key={'tab3'} title="Password" value="tab3" />
-        <TabItem key={'tab4'} title="Team" value="tab4" />
-        <TabItem key={'tab5'} title="Plan" value="tab5" />
-        <TabItem key={'tab6'} title="Billing" value="tab6" />
-        <TabItem key={'tab7'} title="Email" value="tab7" />
-        <TabItem key={'tab8'} title="Notifications" value="tab8" />
-        <TabItem key={'tab9'} title="Integrations" value="tab9" />
-        <TabItem key={'tab10'} title="API" value="tab10" />
+        <TabItem
+          key={'tab1'}
+          title="My Details"
+          value="tab1"
+          isSelected={currentTab === 'tab1'}
+        />
+        <TabItem
+          key={'tab2'}
+          title="Profile"
+          value="tab2"
+          isSelected={currentTab === 'tab2'}
+        />
+        <TabItem
+          key={'tab3'}
+          title="Password"
+          value="tab3"
+          isSelected={currentTab === 'tab3'}
+        />
+        <TabItem
+          key={'tab4'}
+          title="Team"
+          value="tab4"
+          isSelected={currentTab === 'tab4'}
+        />
+        <TabItem
+          key={'tab5'}
+          title="Plan"
+          value="tab5"
+          isSelected={currentTab === 'tab5'}
+        />
+        <TabItem
+          key={'tab6'}
+          title="Billing"
+          value="tab6"
+          isSelected={currentTab === 'tab6'}
+        />
+        <TabItem
+          key={'tab7'}
+          title="Email"
+          value="tab7"
+          isSelected={currentTab === 'tab7'}
+        />
+        <TabItem
+          key={'tab8'}
+          title="Notifications"
+          value="tab8"
+          isSelected={currentTab === 'tab8'}
+        />
+        <TabItem
+          key={'tab9'}
+          title="Integrations"
+          value="tab9"
+          isSelected={currentTab === 'tab9'}
+        />
+        <TabItem
+          key={'tab10'}
+          title="API"
+          value="tab10"
+          isSelected={currentTab === 'tab10'}
+        />
       </Tabs.List>
     </Tabs.Root>
   )
--- src/components/SettingsTabs/tabitem.tsx
+++ src/components/SettingsTabs/tabitem.tsx
@@ -1,5 +1,6 @@
 'use client'
 import * as Tabs from '@radix-ui/react-tabs'
+import { motion } from 'framer-motion'
 export interface TabItemProps {
   value: string
   title: string
@@ -9,11 +10,14 @@ export function TabItem({ value, title, isSelected = false }: TabItemProps) {
   return (
     <Tabs.Trigger
       value={value}
-      className="relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700"
+      className="relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 data-[state=active]:text-violet-700"
     >
       <span>{title}</span>
       {isSelected && (
-        <div className="absolute inset-x-0 -bottom-px h-0.5 border-violet-700" />
+        <motion.div
+          layoutId="activeTab"
+          className="absolute inset-x-0 -bottom-px h-0.5 bg-violet-700"
+        />
       )}
     </Tabs.Trigger>
   )
