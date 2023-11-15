f6d5290015ff60055cd1b39a121555ce308e7a2f
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2023-11-14 23:32:25 -0300

    create photp upload icon and input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import {
   InputControl,
   InputPrefix,
 } from '../components/Sidebar/input'
-import { Mail } from 'lucide-react'
+import { Mail, UploadCloud, User } from 'lucide-react'
 export default function Home() {
   return (
     <>
@@ -78,22 +78,106 @@ export default function Home() {
           <div className="grid grid-cols-form gap-3 pt-5">
             <label
               htmlFor="email"
-              className="text-sm font-medium text-zinc-500"
+              className=" text-sm font-medium text-zinc-500"
             >
-              Your photp
+              Your photo
+              <span className="mt-0.5 block  text-sm font-normal text-zinc-500">
+                This will displaed on your profile.
+              </span>
+            </label>
+            <div className="flex items-start gap-5">
+              <div className="flex h-16 w-16 items-center justify-center rounded-full bg-violet-50">
+                <User className="h-8 w-8 text-violet-500" />
+              </div>
+              <label
+                htmlFor="photo"
+                className="flex flex-1 cursor-pointer flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm"
+              >
+                <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2">
+                  <UploadCloud className="h-5 w-5 text-zinc-600" />
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                  <span>
+                    <span className="font-semibold text-violet-700">
+                      Click to upload
+                    </span>{' '}
+                    or drag and drop
+                  </span>
+                  <span className="text-xs">
+                    SVG,PNG,JPG or GIF(max.800x400px)
+                  </span>
+                </div>
+              </label>
+              <input type="file" name="" id="photo" className="sr-only" />
+            </div>
+          </div>
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label htmlFor="role" className="text-sm font-medium text-zinc-500">
+              Role
             </label>
             <div className="grid grid-cols-2 gap-6">
               <InputRoot>
-                <InputPrefix>
-                  <Mail className="h-5 w-5 text-zinc-500" />
-                </InputPrefix>
-                <InputControl
-                  id="email"
-                  type="email"
-                  defaultValue={'diegofernandes@rocketseat.com'}
-                />
+                <InputControl id="role" defaultValue="CTO" />
               </InputRoot>
             </div>
+          </div>
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label
+              htmlFor="country"
+              className="text-sm font-medium text-zinc-500"
+            >
+              Country
+            </label>
+            <div className="grid grid-cols-2 gap-6">
+              <div></div>
+            </div>
+          </div>
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label
+              htmlFor="timezone"
+              className="text-sm font-medium text-zinc-500"
+            >
+              Timezone
+            </label>
+            <div className="grid grid-cols-2 gap-6">
+              <div></div>
+            </div>
+          </div>
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label htmlFor="bio" className=" text-sm font-medium text-zinc-500">
+              Bio
+              <span className="mt-0.5 block  text-sm font-normal text-zinc-500">
+                Write a short introduction
+              </span>
+            </label>
+            <div className="grid grid-cols-2 gap-6"></div>
+          </div>
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label
+              htmlFor="projects"
+              className=" text-sm font-medium text-zinc-500"
+            >
+              Portfolio projects
+              <span className="mt-0.5 block  text-sm font-normal text-zinc-500">
+                Share a few snippets of your portfolio
+              </span>
+            </label>
+            <div className="grid grid-cols-2 gap-6"></div>
+          </div>
+          <div className="flex items-center justify-end gap-2 pt-5">
+            <button
+              type="button"
+              className="rounded-lg border-zinc-300 px-4 py-2 text-sm font-bold text-zinc-700 shadow-sm hover:bg-zinc-50"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              form="settings"
+              className="rounded-lg bg-violet-700 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
+            >
+              Save
+            </button>
           </div>
         </form>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         profile: 'mincontent 1fr mincontent',
         form: 'minmax(7.5rem,17.5rem) minmax(25rem,1fr) minmax(0, 15rem)',
       },
+      borderWidth: {
+        6: '6px',
+      },
       maxWidth: {
         app: '700px',
       },
